de28d5d51062f388aa5d9fef0c76f6ae754734f9
simonid <woshisimonchen@gmail.com>
2017-11-06 23:06:59 +0800

    add qingting

---
+++ 网页/轻听/download.js
@@ -0,0 +1,133 @@
+MusicJsonCallback({
+	"data": [{
+		"ID": 1,
+		"Ftype": 1,
+		"Ftitle": "Windows PC",
+		"Fversion": "最新版:15.50",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/93507.png",
+		"Fdesc": "五月天林宥嘉田馥甄S.H.E张震岳梁静茹EXO东方神起的歌全都可以听了！\r\n\r\n|我喜欢增加搜索和批量操作功能\r\n|歌词和桌面歌词设置优化\r\n|支持繁体字搜索\r\n|更多性能优化\r\n|发布时间：2017-9-27",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/QQMusicSetup.exe",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 2,
+		"Ftype": 2,
+		"Ftitle": "Mac",
+		"Fversion": "最新版:5.2",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/120987.jpg",
+		"Fdesc": "列表内支持定位到当前播放歌曲\r\n|列表内支持快速搜索歌曲\r\n|用户反馈升级，帮助快速解决问题\r\n|若干性能优化，体验更加流畅\r\n|发布时间：2017-09-22",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/mac/QQMusicMac_Mgr.dmg",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 3,
+		"Ftype": 3,
+		"Ftitle": "iPhone",
+		"Fversion": "最新版:7.9.1",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450856876852257640.jpg",
+		"Fdesc": "iOS 11支持锁屏歌词\r\n|修复了一些已知问题\r\n|发布时间：2017-10-16",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/ios/QQMusicIPhone5.8.1build04_yqq.ipa",
+		"Flink2": "https://itunes.apple.com/cn/app/qq-yin-le/id414603431?mt=8",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450856876852257641.jpg"
+	}, {
+		"ID": 4,
+		"Ftype": 4,
+		"Ftitle": "Android Phone",
+		"Fversion": "最新版:7.9.1.7",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450859070211242470.jpg",
+		"Fdesc": "修复了一些已知问题\r\n|发布时间：2017-10-13",
+		"Flink1": "http://dldir1.qq.com/music/clntupate/QQMusic72282.apk",
+		"Flink2": "",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450859070211242471.jpg"
+	}, {
+		"ID": 5,
+		"Ftype": 5,
+		"Ftitle": "Windows Phone",
+		"Fversion": "最新版:2.4.1",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1452566131404237650.jpg",
+		"Fdesc": "新增歌词海报，分享音乐感动|\n升级登录组件|\nfix bug|\n发布时间：2015-10-26",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/QQMusic_Release_ARM_0113_newHttp.xap",
+		"Flink2": "http://www.windowsphone.com/zh-cn/store/app/qq%E9%9F%B3%E4%B9%90/bb8d2603-091e-43e4-9857-651b4d38b6b4",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450859187135257641.jpg"
+	}, {
+		"ID": 6,
+		"Ftype": 6,
+		"Ftitle": "iPad",
+		"Fversion": "最新版:5.2.1",
+		"Fpic": "",
+		"Fdesc": "微信登录 畅享1500万正版曲库\r\n|独家音效 SuperSound震撼听觉体验\r\n|发布时间：2016-10-18",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/ios/QQMusicPad_5.0build03_yqq.ipa",
+		"Flink2": "https://itunes.apple.com/cn/app/qq-yin-lehd-du-bo-zhong-guo/id429885089?l=en&mt=8",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450921611819202070.png"
+	}, {
+		"ID": 7,
+		"Ftype": 7,
+		"Ftitle": "Android Pad",
+		"Fversion": "最新版:4.8.1",
+		"Fpic": "",
+		"Fdesc": "接入QQ快速登录\r\n|修复已知bug\r\n|发布时间：2016-12-29",
+		"Flink1": "https://dldir1.qq.com/music/clntupate/qqmusichd_4.8.1.2_android_r51397_20161229112446.apk",
+		"Flink2": "",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1452221575587274860.png"
+	}, {
+		"ID": 8,
+		"Ftype": 8,
+		"Ftitle": "腾讯视频PC客户端",
+		"Fversion": "最新版:9.11",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450942983797202070.png",
+		"Fdesc": "支持windows2000及以上|\n发布时间：2015-12-08",
+		"Flink1": "https://dldir1.qq.com/qqtv/qqmusic/QQLiveForYQQ.exe",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 9,
+		"Ftype": 8,
+		"Ftitle": "QQ影音PC客户端",
+		"Fversion": "最新版:2.3",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1450942991801201750.png",
+		"Fdesc": "支持windows2000及以上|\n发布时间：2015-07-01",
+		"Flink1": "https://dldir1.qq.com/qqyy/pc/tuiguang/QQPlayerForYQQ_39_923.exe",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 10,
+		"Ftype": 9,
+		"Ftitle": "Android TV",
+		"Fversion": "最新版:2.2",
+		"Fpic": "",
+		"Fdesc": "音乐界面，全新焕新\r\n|常见问题，意见反馈一键直达\r\n|发布时间：2017-7-26",
+		"Flink1": "https://dldir1.qq.com/music/mobile/qqmusictv_2.2.0.2_android_10007025.apk",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 11,
+		"Ftype": 8,
+		"Ftitle": "腾讯电脑管家",
+		"Fversion": "",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/1462332560077011550.png",
+		"Fdesc": "",
+		"Flink1": "http://dlied6.qq.com/invc/qqpcmgr/qudao/qqpcmgr_v11.5.17490.219_78632_Normal.exe",
+		"Flink2": "",
+		"Fcode": ""
+	}, {
+		"ID": 12,
+		"Ftype": 4,
+		"Ftitle": "轻听",
+		"Fversion": "最新版:1.2.1.3",
+		"Fpic": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/44130.apk",
+		"Fdesc": "期待一款纯粹、美观、小巧、快捷的Android本地播放器吗？让轻听用音乐带你飞吧。\r\n支持Android 4.0以上系统。无任何广告，专注于打造最佳的本地音乐扫描、匹配、管理和播放体验。",
+		"Flink1": "http://pkg.stream.qqmusic.qq.com/qingting_android.apk",
+		"Flink2": "",
+		"Fcode": "https://y.gtimg.cn/music/common/upload/T_Y_SOFTWARE/20511.png"
+	}, {
+		"ID": 14,
+		"Ftype": 10,
+		"Ftitle": "Android Auto",
+		"Fversion": "最新版:1.3",
+		"Fpic": "",
+		"Fdesc": "针对车机硬件，车载场景设计\r\n|歌曲下载，本地歌曲检索排序\r\n|发布时间：2017-9-30",
+		"Flink1": "http://dldir1.qq.com/music/mobile/qqmusiccar_1.3.0.14_android.apk",
+		"Flink2": "",
+		"Fcode": ""
+	}]
+}) /*  |xGv00|c2ca51cf8d8d9524cc9989e9289c6ec1 */
